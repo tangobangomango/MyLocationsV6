56f80cb13379cded9fa0c99329999722f4fe5d6a
Ed Katzman <edkatzman@eds-mac-mini.local>
2018-06-28 09:18:56 -0500

    Initial Commit

---
+++ MyLocationsV6/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocationsV6
+//
+//  Created by Ed Katzman on 6/28/18.
+//  Copyright © 2018 TDG. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyLocationsV6/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyLocationsV6
+//
+//  Created by Ed Katzman on 6/28/18.
+//  Copyright © 2018 TDG. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
